de5e7a6b3048835fa4a24ff83fca3bad52355b87
Amir Tahvildaran <amirdt22@gmail.com>
2020-07-21 21:49:49 -0400

    fix(x11): allow x11 tunneling on wsl

---
+++ zsh/display.zsh
@@ -0,0 +1,5 @@
+# https://stackoverflow.com/questions/61110603/how-to-set-up-working-x11-forwarding-on-wsl2
+export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+export LIBGL_ALWAYS_INDIRECT=1
+
+export LC_ALL=C
